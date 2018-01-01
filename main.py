788b59e6d74301017f5ea0949bab6ef41d665434
Youngbok Yoon <master@bluehack.net>
2018-01-02 00:59:26 +0900

    [BL-274] Preparing bl:image:index:queue with object_id

--- main.py
+++ main.py
@@ -6,9 +6,7 @@ from multiprocessing import Process
 
 import redis
 import time
-import pickle
 from bluelens_spawning_pool import spawning_pool
-from stylelens_image.images import Images
 from stylelens_product.products import Products
 from stylelens_product.crawls import Crawls
 from stylelens_object.objects import Objects
@@ -28,8 +26,6 @@ PRODUCT_NO = 'product_no'
 MAIN = 'main'
 NATION = 'nation'
 
-SPAWN_MAX = 200
-
 REDIS_OBJECT_INDEX_DONE = 'bl:object:index:done'
 REDIS_OBJECT_INDEX_QUEUE = 'bl:object:index:queue'
 REDIS_IMAGE_INDEX_QUEUE = 'bl:image:index:queue'
@@ -44,6 +40,8 @@ DB_INDEX_PORT = os.environ['DB_INDEX_PORT']
 DB_INDEX_NAME = os.environ['DB_INDEX_NAME']
 DB_INDEX_USER = os.environ['DB_INDEX_USER']
 DB_INDEX_PASSWORD = os.environ['DB_INDEX_PASSWORD']
+
+MAX_PROCESS_NUM = int(os.environ['MAX_PROCESS_NUM'])
 
 DB_OBJECT_HOST = os.environ['DB_OBJECT_HOST']
 DB_OBJECT_PORT = os.environ['DB_OBJECT_PORT']
@@ -61,6 +59,11 @@ options = {
 }
 log = Logging(options, tag='bl-image-index')
 
+
+product_api = None
+object_api = None
+image_api = None
+
 def get_latest_crawl_version(rconn):
   value = rconn.hget(REDIS_CRAWL_VERSION, REDIS_CRAWL_VERSION_LATEST)
   log.debug(value)
@@ -71,28 +74,6 @@ def get_latest_crawl_version(rconn):
     version_id = None
   return version_id
 
-def query(version_id):
-  log.info('start query: ' + version_id)
-
-  image_api = Images()
-
-  q_offset = 0
-  q_limit = 100
-
-  try:
-    while True:
-      res = image_api.get_images(version_id=version_id,
-                                 offset=q_offset, limit=q_limit)
-      push_images_to_queue(res)
-      if q_limit > len(res):
-        break
-      else:
-        q_offset = q_offset + q_limit
-  except Exception as e:
-    log.error(str(e))
-
-  cleanup_images(image_api, version_id)
-
 def cleanup_images(image_api, version_id):
   try:
     res = image_api.delete_images(version_id=version_id, except_version=True)
@@ -100,14 +81,12 @@ def cleanup_images(image_api, version_id):
   except Exception as e:
     log.error(e)
 
-def push_images_to_queue(images):
-  rconn.lpush(REDIS_IMAGE_INDEX_QUEUE, pickle.dumps(images))
-
-def spawn_indexer(uuid):
+def spawn(version_id):
+  spawn_id = str(uuid.uuid4())
 
   pool = spawning_pool.SpawningPool()
 
-  project_name = 'bl-image-indexer-' + uuid
+  project_name = 'bl-image-indexer-' + spawn_id
   log.debug('spawn_image-indexer: ' + project_name)
 
   pool.setServerUrl(REDIS_SERVER)
@@ -118,14 +97,16 @@ def spawn_indexer(uuid):
   pool.setMetadataNamespace(RELEASE_MODE)
   pool.addMetadataLabel('name', project_name)
   pool.addMetadataLabel('group', 'bl-image-indexer')
-  pool.addMetadataLabel('SPAWN_ID', uuid)
+  pool.addMetadataLabel('SPAWN_ID', spawn_id)
   container = pool.createContainer()
   pool.setContainerName(container, project_name)
   pool.addContainerEnv(container, 'AWS_ACCESS_KEY', AWS_ACCESS_KEY)
   pool.addContainerEnv(container, 'AWS_SECRET_ACCESS_KEY', AWS_SECRET_ACCESS_KEY)
   pool.addContainerEnv(container, 'REDIS_SERVER', REDIS_SERVER)
   pool.addContainerEnv(container, 'REDIS_PASSWORD', REDIS_PASSWORD)
-  pool.addContainerEnv(container, 'SPAWN_ID', uuid)
+  pool.addContainerEnv(container, 'SPAWN_ID', spawn_id)
+  pool.addContainerEnv(container, 'VERSION_ID', version_id)
+  pool.addContainerEnv(container, 'MAX_PROCESS_NUM', MAX_PROCESS_NUM)
   pool.addContainerEnv(container, 'RELEASE_MODE', RELEASE_MODE)
   pool.addContainerEnv(container, 'DB_INDEX_HOST', DB_INDEX_HOST)
   pool.addContainerEnv(container, 'DB_INDEX_PORT', DB_INDEX_PORT)
@@ -142,8 +123,51 @@ def spawn_indexer(uuid):
   pool.setRestartPolicy('Never')
   pool.spawn()
 
+def prepare_objects_to_index(rconn, version_id):
+  global object_api
+  offset = 0
+  limit = 100
+
+  try:
+    while True:
+      res = object_api.get_objects(version_id=version_id,
+                                   is_indexed=True,
+                                   offset=offset,
+                                   limit=limit)
+      log.debug("Got " + str(len(res)) + ' products')
+      for object in res:
+        rconn.lpush(REDIS_IMAGE_INDEX_QUEUE, str(object['_id']))
+
+      if limit > len(res):
+        break
+      else:
+        offset = offset + limit
+
+  except Exception as e:
+    log.error(str(e))
+
+def dispatch(rconn, version_id):
+  size = rconn.llen(REDIS_IMAGE_INDEX_QUEUE)
+
+  if size < MAX_PROCESS_NUM:
+    for i in range(10):
+      spawn(str(uuid.uuid4()))
+    time.sleep(60*60*2)
+
+  if size >= MAX_PROCESS_NUM and size < MAX_PROCESS_NUM*10:
+    for i in range(300):
+      spawn(str(uuid.uuid4()))
+    time.sleep(60*60*5)
+
+  elif size >= MAX_PROCESS_NUM*100:
+    for i in range(500):
+      spawn(str(uuid.uuid4()))
+    time.sleep(60*60*10)
 
 def check_condition_to_start(version_id):
+  global product_api
+  global object_api
+
   product_api = Products()
   crawl_api = Crawls()
   object_api = Objects()
@@ -176,29 +200,20 @@ def check_condition_to_start(version_id):
 
   return True
 
-def dispatch_query_job(rconn):
+def start(rconn):
   while True:
     version_id = get_latest_crawl_version(rconn)
     if version_id is not None:
       ok = check_condition_to_start(version_id)
       if ok is True:
-        query(version_id)
+        prepare_objects_to_index(rconn, version_id)
+        dispatch(rconn, version_id)
       else:
         time.sleep(60*10)
 
-def dispatch_indexer(rconn):
-
-  count = 0
-  while True:
-    len = rconn.llen(REDIS_IMAGE_INDEX_QUEUE)
-    if len > 0 and count < SPAWN_MAX:
-      spawn_indexer(str(uuid.uuid4()))
-      count = count + 1
-    time.sleep(60 + count * 10)
-
 if __name__ == '__main__':
   try:
-    Process(target=dispatch_query_job, args=(rconn,)).start()
-    Process(target=dispatch_indexer, args=(rconn,)).start()
+    log.info('Start bl-image-index:1')
+    Process(target=start, args=(rconn,)).start()
   except Exception as e:
     log.error(str(e))
